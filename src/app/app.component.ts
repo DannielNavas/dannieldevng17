3a4c1ddcef9b039e67b40c9144f65e9af7251d3f
Danniel Navas <dannielnavas@gmail.com>
2024-01-21 21:08:25 -0500

    ✨ Update styles in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,22 @@ import { NavbarComponent } from './domains/shared/components/navbar/navbar.compo
     <router-outlet />
     <app-footer />
   </div>`,
-  styles: `.container {
-  width: 100%;
-  max-width: 1280px;
-  margin: 0 auto;
-}`,
+  styles: `
+  @import './domains/shared/utils/scss/index.scss';
+  .container {
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    @include for-tablet-portrait-up {
+      width: calc(100% - 32px);
+      padding: 0 16px;
+    }
+
+    @include for-phone-only {
+      width: calc(100% - 32px);
+      padding: 0 16px;
+    }
+  }
+`,
 })
 export class AppComponent {}
