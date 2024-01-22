89a83746bfac185eecf0547525d7c70a668effbf
Danniel Navas <dannielnavas@gmail.com>
2024-01-21 21:11:16 -0500

    ✨ Add article component with HTML, SCSS, and TypeScript files

---
+++ src/app/domains/post/pages/article/article.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, inject } from '@angular/core';
+import { DevtoPostService } from '@domain/shared/services/devto-post/devto-post.service';
+import { MarkdownModule } from 'ngx-markdown';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [MarkdownModule],
+  providers: [],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.scss',
+})
+export class ArticleComponent {
+  @Input() id?: string;
+  private devtoPostService = inject(DevtoPostService);
+  response: any;
+
+  ngOnInit(): void {
+    this.getArticle();
+  }
+
+  getArticle(): void {
+    this.devtoPostService.article(this.id ?? '').subscribe({
+      next: (response) => {
+        console.log(response);
+        this.response = response;
+      },
+      error: (error) => {
+        throw new Error(error);
+      },
+    });
+  }
+
+  onCopyToClipboard() {
+    alert('copiado');
+  }
+}
