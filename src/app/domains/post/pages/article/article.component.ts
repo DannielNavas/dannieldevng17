ad92d73de94ff55e4da60143d92505e6a449d531
Danniel Navas <dannielnavas@gmail.com>
2024-01-25 20:57:54 -0500

    ✨ Update article component imports and providers

--- src/app/domains/post/pages/article/article.component.ts
+++ src/app/domains/post/pages/article/article.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input, inject } from '@angular/core';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { Component, Input, LOCALE_ID, inject } from '@angular/core';
 import { Article } from '@domain/shared/models/article.interface';
 import { DevtoPostService } from '@domain/shared/services/devto-post/devto-post.service';
 import { MarkdownModule } from 'ngx-markdown';
 
+registerLocaleData(localeEs, 'es');
+
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [MarkdownModule],
-  providers: [],
+  imports: [MarkdownModule, DatePipe],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-CO',
+    },
+  ],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss',
 })
@@ -23,7 +32,6 @@ export class ArticleComponent {
   getArticle(): void {
     this.devtoPostService.article(this.id ?? '').subscribe({
       next: (response) => {
-        console.log(response);
         this.response = response;
       },
       error: (error) => {
